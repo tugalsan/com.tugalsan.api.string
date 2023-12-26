b6759dae88e18ff7b08099caf57c021b6baf2cfc
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-12-26 11:45:06 +0300

    Update TGS_StringDouble.java

--- src/main/java/com/tugalsan/api/string/client/TGS_StringDouble.java
+++ src/main/java/com/tugalsan/api/string/client/TGS_StringDouble.java
@@ -3,6 +3,8 @@ package com.tugalsan.api.string.client;
 import com.tugalsan.api.charset.client.TGS_CharSetCast;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TGS_StringDouble {
 
@@ -19,19 +21,21 @@ public class TGS_StringDouble {
     }
 
     public double val() {
-        return Double.parseDouble(left + "." + right);
+        return Double.parseDouble(left + "." + IntStream.range(0, right_zero_onTheFront).mapToObj(i -> "0").collect(Collectors.joining()) + right);
     }
 
-    private TGS_StringDouble(long left, long right, TGS_CharSetCast.Locale2Cast locale2Cast) {
+    private TGS_StringDouble(long left, long right, int right_zero_onTheFront, TGS_CharSetCast.Locale2Cast locale2Cast) {
         this.left = left;
         this.right = right;
+        this.right_zero_onTheFront = right_zero_onTheFront;
         this.locale2Cast = locale2Cast;
     }
     final public long left, right;
+    final public int right_zero_onTheFront;
     final public TGS_CharSetCast.Locale2Cast locale2Cast;
 
-    public static TGS_StringDouble of(long left, long right, TGS_CharSetCast.Locale2Cast locale2Cast) {
-        return new TGS_StringDouble(left, right, locale2Cast);
+    public static TGS_StringDouble of(long left, long right, int right_zero_onTheFront, TGS_CharSetCast.Locale2Cast locale2Cast) {
+        return new TGS_StringDouble(left, right, right_zero_onTheFront, locale2Cast);
     }
 
     public static Optional<TGS_StringDouble> of(CharSequence inputText) {
@@ -51,17 +55,19 @@ public class TGS_StringDouble {
         }
         //FETCH LEFT
         var left = internationalText.substring(0, idx);
-        var leftLng = TGS_UnSafe.call(() -> Long.parseLong(left), e -> null);
+        var leftLng = TGS_UnSafe.call(() -> Long.valueOf(left), e -> null);
         if (leftLng == null) {
             return Optional.empty();
         }
         //FETCH RIGHT
         var right = internationalText.substring(idx + 1);
-        var rightLng = TGS_UnSafe.call(() -> Long.parseLong(right), e -> null);
+        var rightLng = TGS_UnSafe.call(() -> Long.valueOf(right), e -> null);
         if (rightLng == null) {
             return Optional.empty();
         }
-        var obj = of(leftLng, rightLng, locale2Set);
+        //CALC right_zero_onTheFront
+        var right_zero_onTheFront = right.length() - String.valueOf(rightLng).length();
+        var obj = of(leftLng, rightLng, right_zero_onTheFront, locale2Set);
         return TGS_UnSafe.call(() -> {
             obj.val();//if not throws
             return Optional.of(obj);
