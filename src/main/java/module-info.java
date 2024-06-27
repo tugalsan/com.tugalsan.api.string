fd58d6176d69ecc8d8b667160af7f34ef43b59db
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 11:58:58 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.tugalsan.api.string {
     requires gwt.user;
     requires com.tugalsan.api.unsafe;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.charset;
     requires com.tugalsan.api.union;
     exports com.tugalsan.api.string.client;
